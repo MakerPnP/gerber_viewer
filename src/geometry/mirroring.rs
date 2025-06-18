19cc306ed9d855e7274bc837477aedc926e15f00
Dominic Clifton <me@dominicclifton.name>
2025-06-18 06:35:38 +0200

    Expand mirroring API.

--- src/geometry/mirroring.rs
+++ src/geometry/mirroring.rs
@@ -35,6 +35,56 @@ impl From<[bool; 2]> for Mirroring {
     }
 }
 
+impl From<[i8; 2]> for Mirroring {
+    fn from(value: [i8; 2]) -> Self {
+        Self {
+            x: value[0] != 0,
+            y: value[1] != 0,
+        }
+    }
+} 
+
+impl From<(bool, bool)> for Mirroring {
+    fn from(value: (bool, bool)) -> Self {
+        Self {
+            x: value.0,
+            y: value.1,
+        }
+    }
+}
+
+impl From<(i8, i8)> for Mirroring {
+    fn from(value: (i8, i8)) -> Self {
+        Self {
+            x: value.0 != 0,
+            y: value.1 != 0,
+        }
+    }
+}
+
+impl Mirroring {
+    pub fn as_f64(&self) -> [f64; 2] {
+        [
+            if self.x { -1.0 } else { 1.0 },
+            if self.y { -1.0 } else { 1.0 },
+        ]
+    }
+    
+    pub fn as_f32(&self) -> [f32; 2] {
+        [
+            if self.x { -1.0 } else { 1.0 },
+            if self.y { -1.0 } else { 1.0 },
+        ]
+    }
+    
+    pub fn as_i8(&self) -> [i8; 2] {
+        [
+            if self.x { -1 } else { 1 },
+            if self.y { -1 } else { 1 },
+        ]
+    }
+}
+
 #[cfg(feature = "egui")]
 impl From<Vec2b> for Mirroring {
     fn from(value: Vec2b) -> Self {
